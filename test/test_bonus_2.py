099b1c9edd5c340be1a6b6f2beb1479f6c22fe62
Marco William Silva <marcowilliamdf@gmail.com>
2019-10-21 12:56:11 -0600

    Create test for bonus 2 question

---
+++ test/test_bonus_2.py
@@ -0,0 +1,11 @@
+import unittest
+from calculator.Calculator import Calculator
+
+class CalculatorTestBonus2(unittest.TestCase):
+    calculator = Calculator()
+    def test_add(self):
+        self.assertEqual(6, self.calculator.add("//***\n1***2***3"))
+
+
+if __name__ == "__main__":
+    unittest.main()
