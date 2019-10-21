781c1948cea5d87c242be6e2e31be2a401f87526
Marco William Silva <marcowilliamdf@gmail.com>
2019-10-21 12:30:06 -0600

    Create exercise 1

--- calculator/Calculator.py
+++ calculator/Calculator.py
@@ -1,2 +1,12 @@
 class Calculator:
-    pass
+
+    def add(self, numbers):
+        if numbers.strip():
+            splitted_numbers = map(int, numbers.split(","))
+            return sum(splitted_numbers)
+        else:
+            return 0
+            
+if __name__ == '__main__':
+    numbers = input("Enter string of numbers: ")
+    print Calculator().add(numbers)
--- test/test_exercise_1.py
+++ test/test_exercise_1.py
@@ -2,7 +2,15 @@ import unittest
 from calculator.Calculator import Calculator
 
 class CalculatorTestExercise1(unittest.TestCase):
-    pass
+    calculator = Calculator()
+    def test_add(self):
+        self.assertEqual(8, self.calculator.add("1,2,5"))
+
+    def test_empty_string(self):
+        self.assertEqual(0, self.calculator.add(""))
+
+    def test_returns_int(self):
+        self.assertIsInstance(self.calculator.add("1,2,5"), int)
 
 if __name__ == "__main__":
     unittest.main()
